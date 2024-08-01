83bfc82d93dbd84944d9be1d5353aa2d2d68cb62
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2024-08-01 22:49:36 +0530

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import SharedCart from './components/SharedCart/SharedCart.jsx'
 import './index.css'
 import {
   createBrowserRouter,RouterProvider
 } from "react-router-dom";
-import SharedCart from './components/sharedCart/SharedCart';
+
 
 
 
