bbf4ed0203d2c718fdd7c74bf8b258f203348492
Rishabh Ranjan <ranjan.rishabh0902@gmail.com>
2024-08-11 22:34:32 +0530

    UPD: added images

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -7,13 +7,43 @@ function Hero() {
     <>
      <Navbar/>
         <div className={styles.mainDiv}>
-          <div className={`${styles.item1} ${styles.item}`} ></div>
-          <div className={`${styles.item2} ${styles.item}`}></div>
-          <div className={`${styles.item3} ${styles.item}`}></div>
-          <div className={`${styles.item4} ${styles.item}`}></div>
-          <div className={`${styles.item5} ${styles.item}`}></div>
-          <div className={`${styles.item6} ${styles.item}`}></div>
-          <div className={`${styles.item7} ${styles.item}`}></div>
+          <div className={`${styles.item1} ${styles.item}`} >
+            <img src="https://res.cloudinary.com/dp3aoinmu/image/upload/v1723389449/Walmart_PNG/cdkzlgxeoskx2gh5i0ia.jpg" alt="" />
+            <div className={styles.text}>
+              <h2>Stay Hydrated, <br /> Stay Happy</h2>
+            </div>
+          </div>
+          <div className={`${styles.item2} ${styles.item}`}>
+          <div className={styles.text}>
+              <h2>Stay Hydrated, <br /> Stay Happy</h2>
+            </div>
+          </div>
+          <div className={`${styles.item3} ${styles.item}`}>
+          <div className={styles.text}>
+              <h2>Stay Hydrated, <br /> Stay Happy</h2>
+            </div>
+          </div>
+          <div className={`${styles.item4} ${styles.item}`}>
+          <div className={styles.text}>
+              <h2>Stay Hydrated, <br /> Stay Happy</h2>
+            </div>
+          </div>
+          <div className={`${styles.item5} ${styles.item}`}>
+            <img src="https://res.cloudinary.com/dp3aoinmu/image/upload/v1723391534/Walmart_PNG/pk5coqa8qg0hpnzm5t6h.jpg" alt="" />
+          <div className={styles.text}>
+              <h2>Thocky Keyboard</h2>
+            </div>
+          </div>
+          <div className={`${styles.item6} ${styles.item}`}>
+          <div className={styles.text}>
+              <h2>Stay Hydrated, <br /> Stay Happy</h2>
+            </div>
+          </div>
+          <div className={`${styles.item7} ${styles.item}`}>
+          <div className={styles.text}>
+              <h2>Stay Hydrated, <br /> Stay Happy</h2>
+            </div>
+          </div>
         </div>
     </>
     
