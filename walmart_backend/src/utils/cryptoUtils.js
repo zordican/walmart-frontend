44e20b14eae05d3db2fe22b1663dc398ee1c8fa6
Aryan Yadav <74095206+Sandrocottus1@users.noreply.github.com>
2024-08-11 20:41:22 +0530

    Delete walmart_backend directory

--- walmart_backend/src/utils/cryptoUtils.js
+++
@@ -1,9 +0,0 @@
-import * as crypto from 'crypto';
-
-export function generateSalt() {
-  return crypto.randomBytes(16).toString('hex');
-}
-
-export function hashPassword(password, salt) {
-  return crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
-}
