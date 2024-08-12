6b9520042bb7691b284b717fb20e3c416d7c22ab
Aryan Yadav <74095206+Sandrocottus1@users.noreply.github.com>
2024-08-12 13:38:56 +0530

    back-end

--- walmart_backend/src/controllers/authController.js
+++ walmart_backend/src/controllers/authController.js
@@ -8,8 +8,9 @@ export const signup = async (req, res) => {
   try {
     const existingUser = await prisma.user.findUnique({ where: { email } });
     if (existingUser) {
-      return res.redirect('/login');
+      return res.status(400).json({ message: 'User already exists. Please log in.' });
     }
+
     const salt = generateSalt();
     const hashedPassword = hashPassword(password, salt);
 
@@ -25,10 +26,10 @@ export const signup = async (req, res) => {
     const token = jwt.sign({ userId: newUser.id }, process.env.JWT_SECRET, { expiresIn: '24h' });
     res.cookie('jwt', token, { httpOnly: true, maxAge: 3600000 });
 
-    return res.json({ message: 'User signed up successfully', user: newUser, token : token,});
+    return res.json({ message: 'User signed up successfully', user: newUser, token });
   } catch (error) {
-    console.log(error);
-    return res.json({ error });
+    console.error('Signup Error:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
@@ -58,7 +59,7 @@ export const login = async (req, res) => {
     // Check if the user has a previously generated shared cart
     const userCart = await prisma.userCart.findFirst({
       where: { userId: existingUser.id },
-      include: { cart: true }
+      include: { cart: true },
     });
 
     let cartId = null;
@@ -66,27 +67,26 @@ export const login = async (req, res) => {
       cartId = userCart.cart.id;
     }
 
-    return res.json({ 
-      message: 'Login successful', 
-      user: existingUser, 
-      token: token, 
-      cartId: cartId 
+    return res.json({
+      message: 'Login successful',
+      user: existingUser,
+      token,
+      cartId,
     });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Login Error:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
 export const logout = (req, res) => {
-  res.clearCookie('jwt');
+  res.clearCookie('jwt', { path: '/' });
   return res.json({ message: 'Logout successful' });
 };
 
-
 export const checkAuth = async (req, res) => {
   const token = req.cookies.jwt;
-  
+
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -97,6 +97,7 @@ export const checkAuth = async (req, res) => {
         return res.json({ user });
       }
     } catch (error) {
+      console.error('Token Verification Error:', error);
       return res.status(401).json({ message: 'Unauthorized' });
     }
   }
